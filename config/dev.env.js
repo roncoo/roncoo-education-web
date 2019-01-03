fa51dcfcf8833e488692b70f048d31a3752f313d
laiwenbiao <laiwb@roncoo.com>
2019-01-03 15:38:59 +0800

    domain

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,9 +5,9 @@ export default {
         id: 'lkb65617f842ad4c37895a733b8de43cbb',
         keys: '6ce6c389407549b18f147cb0f5543dfc',
         name: '领课学院',
-        domain: 'localhost',
+        domain: 'vicp.net',
         tokenName: 'OSTK',
-        mainUrl: 'http://localhost:3000',
+        mainUrl: 'http://roncoo.vicp.net',
         accountUrl: 'http://localhost:8080'
     }
 }
--- config/prod.env.js
+++ config/prod.env.js
@@ -5,9 +5,9 @@ export default {
         id: 'lkb65617f842ad4c37895a733b8de43cbb',
         keys: '6ce6c389407549b18f147cb0f5543dfc',
         name: '领课学院',
-        domain: 'roncoo.vicp.net',
+        domain: 'roncoo.com',
         tokenName: 'OSTK',
-        mainUrl: 'http://roncoo.vicp.net',
-        accountUrl: 'http://roncoo.vicp.net'
+        mainUrl: 'http://edu.os.roncoo.com',
+        accountUrl: 'http://account.edu.roncoo.com'
     }
 }
