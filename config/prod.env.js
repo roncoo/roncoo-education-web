4e3b3690fb4e29342a7a9125429670e66332ea4a
laiwenbiao <laiwb@roncoo.com>
2019-01-03 15:35:11 +0800

    domain

--- config/prod.env.js
+++ config/prod.env.js
@@ -5,9 +5,9 @@ export default {
         id: 'lkb65617f842ad4c37895a733b8de43cbb',
         keys: '6ce6c389407549b18f147cb0f5543dfc',
         name: '领课学院',
-        domain: 'vicp.net',
+        domain: 'roncoo.vicp.net',
         tokenName: 'OSTK',
-        mainUrl: 'http://edu.os.roncoo.com',
-        accountUrl: 'http://account.edu.roncoo.com'
+        mainUrl: 'http://roncoo.vicp.net',
+        accountUrl: 'http://roncoo.vicp.net'
     }
 }
