1e8402428c319db22ccb18a56b7f6fa5550ae0d2
liumj <lmj@roncoo.com>
2019-01-07 09:56:42 +0800

    登录

--- config/test.env.js
+++ config/test.env.js
@@ -5,7 +5,7 @@ export default {
         id: 'lkb65617f842ad4c37895a733b8de43cbb',
         keys: '6ce6c389407549b18f147cb0f5543dfc',
         name: '领课学院',
-        domain: 'vicp.net',
+        domain: 'roncoo.vicp.net',
         tokenName: 'OSTK',
         mainUrl: 'http://roncoo.vicp.net',
         accountUrl: 'http://localhost:8080'
