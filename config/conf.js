dbc86ffee4f4e05a557664ac83694396d8acae43
laiwenbiao <laiwb@roncoo.com>
2019-01-30 11:12:17 +0800

    开发baseurl

--- config/conf.js
+++ config/conf.js
@@ -1,16 +1,16 @@
 if (process.env.NODE_ENV == 'development') {
   module.exports =  {
     title: '领课学院',
-    baseUrl: 'http://192.168.1.181:5840/api'
+    baseUrl: 'http://roncoo.vicp.net/api'   //java项目接口地址
   }
 }else if (process.env.NODE_ENV == 'testing') {  
   module.exports =  {
     title: '领课学院',
-    baseUrl: 'http://192.168.1.181:5840/api'
+    baseUrl: 'http://192.168.1.181:5840/api'  //java项目接口地址
   }
 }else {
   module.exports =  {
     title: '领课学院',
-    baseUrl: 'http://roncoo.vicp.net/api'
+    baseUrl: 'http://roncoo.vicp.net/api'   //java项目接口地址
   }
 }
