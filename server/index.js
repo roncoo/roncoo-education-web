cc733922b707f44d0417c0c445acb2a0326c1761
laiwenbiao <laiwb@roncoo.com>
2019-02-27 16:49:01 +0800

    删除nuxtjs默认host

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 
 const app = new Koa()
-const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
 // Import and Set Nuxt.js options
@@ -33,9 +32,9 @@ async function start() {
     })
   })
 
-  app.listen(port, host)
+  app.listen(port)
   consola.ready({
-    message: `Server listening on http://${host}:${port}`,
+    message: `Server listening on http://127.0.0.1:${port}`,
     badge: true
   })
 }
