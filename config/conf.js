ce23352b14198933c33a1a440db19f21a8f7ab15
fengyongwei <fengyw@roncoo.com>
2020-02-07 16:53:45 +0800

    统一请求路径

--- config/conf.js
+++ config/conf.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV == 'development') { //开发环境
   module.exports =  {
     title: '领课学院',
-    baseUrl: 'http://127.0.0.1:5840/'   //java项目接口地址
+    baseUrl: 'http://127.0.0.1:5840/api'   //java项目接口地址
   }
 }else if (process.env.NODE_ENV == 'testing') {  //测试环境
   module.exports =  {
@@ -11,6 +11,6 @@ if (process.env.NODE_ENV == 'development') { //开发环境
 }else { //默认生产环境
   module.exports =  {
     title: '领课学院',
-    baseUrl: 'http://127.0.0.1:5840/'   //java项目接口地址
+    baseUrl: 'http://127.0.0.1:5840/api'   //java项目接口地址
   }
 }
