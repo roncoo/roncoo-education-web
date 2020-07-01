a7fdd435b7b8fd057cf554c9ef37b6029d7e3f94
fengyongwei <fengyw@roncoo.com>
2020-07-01 15:56:49 +0800

    修改标题

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ export default {
     CLIENT: {
         no: 'lingke', // 接口需要的机构号
         id: 'lkb65617f842ad4c37895a733b8de43cbb', // 接口需要的clientId
-        name: '领课学院', // 页面title
+        name: '领课教育系统(开源版)-打造全行业都适用的在线教育系统', // 页面title
         domain: 'localhost', // cookie作用域
         tokenName: 'OSTK', // cookie保存的名称
         mainUrl: 'http://localhost:3000' // 网站域名
