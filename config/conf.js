9523c3abd68b25f27df635ee48739ffcc1e901f9
Administrator <admin@example.com>
2021-05-26 17:23:01 +0800

    Update conf.js

--- config/conf.js
+++ config/conf.js
@@ -6,11 +6,11 @@ if (process.env.NODE_ENV == 'development') { //开发环境
 }else if (process.env.NODE_ENV == 'testing') {  //测试环境
   module.exports =  {
     title: '领课教育系统(开源版)-打造全行业都适用的在线教育系统',
-    baseUrl: 'http://127.0.0.1:5840/api'  //java项目接口地址
+    baseUrl: 'http://127.0.0.1:5840'  //java项目接口地址
   }
 }else { //默认生产环境
   module.exports =  {
     title: '领课教育系统(开源版)-打造全行业都适用的在线教育系统',
-    baseUrl: 'http://127.0.0.1:5840/api'   //java项目接口地址
+    baseUrl: 'http://127.0.0.1:5840'   //java项目接口地址
   }
 }
