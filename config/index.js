70eb84db7e60c2a74f3cb0c4fd2aae3b34d337af
fengyw <fengyw@roncoo.com>
2022-10-18 14:42:50 +0800

    增加网关地址说明

--- config/index.js
+++ config/index.js
@@ -1,12 +1,16 @@
 const result = {
-  baseUrl: 'http://localhost:8180', // 接口地址，生产环境
-  tokenName: 'OSTK' // cookie保存的名称
+  baseUrl: '', // 接口地址
+  tokenName: 'OSTK' // cookie名称
 }
 if (process.env.NODE_ENV === 'development') {
   // 开发环境
-  result.baseUrl = 'https://eduos.roncoo.net/gateway'
+  result.baseUrl = 'http://localhost:8180'
+  // result.baseUrl = 'https://eduos.roncoo.net/gateway'
 } else if (process.env.NODE_ENV === 'testing') {
   // 测试环境
+  result.baseUrl = 'http://localhost:8180'
+} else {
+  // 生产环境
   result.baseUrl = 'http://localhost:8180'
 }
 
