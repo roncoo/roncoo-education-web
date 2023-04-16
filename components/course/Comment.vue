a54a04fe1a84b3715fcd82b837d15791676cfd85
fengyw <fengyw@roncoo.com>
2023-04-16 21:40:34 +0800

    课程收藏和课程评论功能

--- components/course/Comment.vue
+++ components/course/Comment.vue
@@ -2,16 +2,19 @@
 <template>
   <div>
     <div class="comment_box">
-      <textarea
-        v-model="commentText"
-        class="comment_input"
-        rows="4"
-        maxlength="200"
-        placeholder="请输入内容"
-      />
-      <div class="comment_btn_wrapper">
-        <div class="comment_btn" :class="{no_allow: !commentText}" @click="handleComment">
-          评论
+      <textarea v-model="commentText" class="comment_input" rows="4" maxlength="200" placeholder="请输入内容" />
+      <div class="comment_btn_wrapper"><div class="comment_btn" :class="{no_allow: !commentText}" @click="handleComment">评论</div></div>
+      <div class="course_comment_list">
+        <div v-for="(comment, index) in pageObj.list" :key="index" class="course_comment_item">
+          <div class="course_comment_cover"><img :src="comment.usersVO.userHead" class="comment_cover_img"></div>
+          <div class="course_comment_content">
+            <div class="course_comment_user">{{ comment.usersVO.nickname }}</div>
+            <div class="course_comment_int">{{ comment.commentText }}</div>
+            <div class="course_comment_btn"><div class="course_comment_time">{{ comment.gmtCreate }}</div></div>
+          </div>
+        </div>
+        <div class="loading_btn">
+          <span class="loading_text">已经到底部，没有更多内容了</span>
         </div>
       </div>
     </div>
@@ -102,5 +105,42 @@ export default {
   background: #F5F5F5;
   color: #999;
   cursor: not-allowed;
+}
+
+.course_comment_item{
+  border-bottom: 1px solid #ebeef5;
+  display: flex;
+  padding-bottom: 20px;
+  padding-top: 20px;
+}
+.course_comment_cover {
+  margin-right:20px;
+  .comment_cover_img{
+    border-radius:50%;
+    width: 50px;
+  }
+}
+
+.course_comment_content {
+  width: calc(100% - 68px);
+  .course_comment_int{
+  color: #999;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  margin-bottom: 10px;
+  margin-top: 10px;}
+}
+.loading_btn{
+  margin-top: 20px;
+  padding-bottom: 11px;
+  padding-top: 11px;
+  text-align: center;
+}
+.loading_text{
+  color: #999;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
 }
 </style>
