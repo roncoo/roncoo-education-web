2917a4484726ae75181ff4e268705c6a86dab66a
fengyw <fengyw@roncoo.com>
2023-05-06 14:39:30 +0800

    优化配置

--- config/index.js
+++ config/index.js
@@ -1,15 +1,8 @@
 const result = {
-  baseUrl: '', // 接口地址
+  baseUrl: 'http://localhost:8180', // 接口地址
   tokenName: 'OSTK' // cookie名称
 }
-if (process.env.NODE_ENV === 'development') {
-  // 开发环境
-  // result.baseUrl = 'http://localhost:8180'
-  result.baseUrl = 'https://dev-os.roncoos.com/gateway'
-} else if (process.env.NODE_ENV === 'testing') {
-  // 测试环境
-  result.baseUrl = 'http://localhost:8180'
-} else {
+if (process.env.NODE_ENV === 'production') {
   // 生产环境
   result.baseUrl = 'http://localhost:8180'
 }
