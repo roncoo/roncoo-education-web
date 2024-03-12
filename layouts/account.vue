55b5a7dc08ebcc9d35603dd981c2444a60fa1955
fengyw <fengyw@roncoo.com>
2024-03-12 16:19:20 +0800

    feat: 使用nuxt3进行项目重构

--- layouts/account.vue
+++ layouts/account.vue
@@ -5,7 +5,7 @@
       <client-only>
         <el-menu :default-active="defaultActive">
           <nuxt-link v-for="(item, index) in menuList" :key="index" :to="{ name: item.name }">
-            <el-menu-item :index="item.name"> <img src="~/assets/svg/course.svg" class="img-icon" /> {{ item.title }} </el-menu-item>
+            <el-menu-item :index="item.name"> <img :src="item.icon" class="img-icon" /> {{ item.title }} </el-menu-item>
           </nuxt-link>
         </el-menu>
       </client-only>
@@ -20,25 +20,30 @@
   const route = useRoute()
   const defaultActive = ref(route.name)
 
+  import course from '~/assets/svg/course.svg'
+  import collect from '~/assets/svg/collect.svg'
+  import order from '~/assets/svg/order.svg'
+  import user from '~/assets/svg/user.svg'
+
   const menuList = [
     {
       name: 'account-course',
-      icon: '~/assets/svg/course.svg',
+      icon: course,
       title: '我的课程'
     },
     {
       name: 'account-collect',
-      icon: '~/assets/svg/collect.svg',
+      icon: collect,
       title: '我的收藏'
     },
     {
       name: 'account-order',
-      icon: '~/assets/svg/order.svg',
+      icon: order,
       title: '我的订单'
     },
     {
       name: 'account-user',
-      icon: '~/assets/svg/user.svg',
+      icon: user,
       title: '个人信息'
     }
   ]
@@ -49,7 +54,8 @@
     margin: 70px auto 0;
     height: calc(100vh - 130px);
     .img-icon {
-      width: 25px;
+      width: 20px;
+      margin-right: 5px;
     }
   }
   .el-aside {
