b9dfd575d7fba11207c93886b0bc18c64d20498e
fengyw <fengyw@roncoo.com>
2024-06-07 14:34:51 +0800

    feat: 使用nuxt3进行项目重构

--- middleware/router.global.js
+++ middleware/router.global.js
@@ -7,4 +7,16 @@ export default defineNuxtRouteMiddleware((to) => {
   if (to.matched.length === 0) {
     return navigateTo('/404')
   }
+
+  // 判断若是手机端，则跳转手机端路由
+  if (process.client) {
+    const isMobile = /(Android|webOS|iPhone|iPod|tablet|BlackBerry|Mobile)/i.test(navigator.userAgent)
+    if (isMobile) {
+      const isRouterMobile = /h5/i.test(to.fullPath)
+      if (!isRouterMobile) {
+        let path = '/h5' + to.fullPath.slice(0, to.fullPath.length - 1)
+        return navigateTo(path)
+      }
+    }
+  }
 })
