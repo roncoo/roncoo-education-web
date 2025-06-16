944baac60ee5aba8ddaa7f9137c14c007cad028d
fengyw <fengyw@roncoo.com>
2025-06-17 07:39:17 +0800

    doc: 增加说明

--- components/Common/Affix.vue
+++ components/Common/Affix.vue
@@ -3,22 +3,28 @@
     <el-affix class="affix-info">
       <el-popover placement="left">
         <template #reference>
-          <img src="~/assets/svg/gzh.svg" width="30" />
+          <div class="icon">
+            <img src="~/assets/svg/phone.svg" width="30" alt="" />
+            <div>商业版</div>
+          </div>
         </template>
         <template #default>
-          <img style="width: 126px" src="~/assets/image/gzh.jpg" />
-          <div style="font-size: 10px; text-align: center">扫码关注微信公众号</div>
+          <img style="width: 126px" src="~/assets/image/phone.png" alt="" />
+          <div style="font-size: 10px; text-align: center">扫码咨询商业版</div>
         </template>
       </el-popover>
     </el-affix>
     <el-affix class="affix-info">
       <el-popover placement="left">
         <template #reference>
-          <img src="~/assets/svg/phone.svg" width="30" />
+          <div class="icon">
+            <img src="~/assets/svg/gzh.svg" width="30" alt="" />
+            <div>公众号</div>
+          </div>
         </template>
         <template #default>
-          <img style="width: 126px" src="~/assets/image/phone.jpg" />
-          <div style="font-size: 10px; text-align: center">扫码进入移动端学习</div>
+          <img style="width: 126px" src="~/assets/image/gzh.jpg" alt="" />
+          <div style="font-size: 10px; text-align: center">扫码关注微信公众号</div>
         </template>
       </el-popover>
     </el-affix>
@@ -34,5 +40,10 @@
       background-color: #fff;
       padding: 10px;
     }
+  }
+  .icon {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 </style>
